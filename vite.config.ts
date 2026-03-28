fbee833fc01917cd707ba600970e107375278a09
Tu Dang <dangtu06@github.com>
2026-03-28 09:27:32 +0700

    Configure base path for Vercel

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,8 @@ import {defineConfig, loadEnv} from 'vite';
 export default defineConfig(({mode}) => {
   const env = loadEnv(mode, '.', '');
   return {
-    base: '/yeulaichinhminh/',
+    // Tự động rút ngắn đường dẫn nếu deploy trên Vercel
+    base: process.env.VERCEL ? '/' : '/yeulaichinhminh/',
     plugins: [react(), tailwindcss()],
     define: {
       'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
